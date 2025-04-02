d938427c7a05ac165c5fee53ea0c42c276552a4b
Vincent Nguyen <vincen7.nguyen@gmail.com>
2025-04-02 15:40:42 +0200

    change dockerfile and compose
    fixes delays/permissions for joystick

--- docker-run.sh
+++ docker-run.sh
@@ -2,6 +2,8 @@
 docker run -it --rm \
   --privileged \
   --net=host \
-  --device=/dev/input/js0 \
+  -v /dev:/dev/ \
+  --user $(id -u):$(id -g) \
+  -e HOME=/tmp \
   joystick_ros2:latest \
   ros2 launch joy_to_cmd_vel joy_to_cmd_vel_launch.py
